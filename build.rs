ce01f62d1a1f6f0f0633f40c5fc9dd436cb96dff
Priyanshu Singh <anshu.singh1429@gmail.com>
2024-12-14 13:19:23 +0530

    added monoio-transports from crate source, Re-exported the crate
    refactored the feature flags to unify the git and crate source
    
    updated Readme

--- build.rs
+++ build.rs
@@ -1,12 +1,18 @@
 fn main() {
     let monoio_default = cfg!(feature = "monoio-default");
     let monoio_legacy = cfg!(feature = "monoio-legacy");
+    let default_crate = cfg!(feature = "default-crate");
 
+    // Ensure only one of monoio-default or monoio-legacy is enabled
     if monoio_legacy && monoio_default {
-        panic!("The feature 'monoio-legacy' cannot work with 'monoio-default' flag, consider disabling default features");
+        panic!("The feature 'monoio-legacy' cannot work with 'monoio-default'. Consider disabling default features.");
     }
 
-    if !monoio_legacy && !monoio_default {
-        panic!("Atleast one of 'monoio-default' or 'monoio-legacy' feature is required to be enabled");
+    if default_crate && (monoio_default || monoio_legacy) {
+        panic!("The features 'monoio-default' or 'monoio-legacy' cannot be used with 'default-crate'");
+    }
+
+    if !monoio_legacy && !monoio_default && !default_crate {
+        panic!("At least one of 'monoio-default' or 'monoio-legacy' or 'default-crate' must be enabled.");
     }
 }
